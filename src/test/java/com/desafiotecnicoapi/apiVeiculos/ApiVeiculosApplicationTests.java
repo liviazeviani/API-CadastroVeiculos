475783a68946e56124ce494db35e06013ee3589c
liviazeviani <liviapavinizeviani@gmail.com>
2022-06-10 21:23:45 -0300

    Inicializando repositório

---
+++ src/test/java/com/desafiotecnicoapi/apiVeiculos/ApiVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnicoapi.apiVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
